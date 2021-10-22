c1c92817eae8e3a6ebe32af073c21e08abea1318
ismael <asmael-1987@hotmail.com>
2021-10-22 09:46:02 +0200

    adding the spring boot project

---
+++ student/src/main/java/com/example/student/controller/StudentController.java
@@ -0,0 +1,69 @@
+package com.example.student.controller;
+
+import com.example.student.exception.ResourceNotFoundException;
+import com.example.student.model.Student;
+import com.example.student.repo.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    //getAll student
+    @GetMapping("/students")
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    //create Student
+    @PostMapping("/students")
+    public Student addNewStudent(@RequestBody Student student){
+        return studentRepository.save(student);
+    }
+
+    //get student by id
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student>  getStudentById(@PathVariable Long id){
+        Student student = studentRepository
+                .findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Student not found with id :" +id));
+        return ResponseEntity.ok(student);
+    }
+
+    //edit student by id
+    @PutMapping("/students/{id}")
+    public ResponseEntity<Student>  editStudentById(@PathVariable Long id, @RequestBody Student studentDetails){
+        Student student = studentRepository
+                .findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Student not found with id :" +id));
+        student.setFirstName(studentDetails.getFirstName());
+        student.setFirstName(studentDetails.getLastName());
+        student.setFirstName(studentDetails.getSchoolClass());
+        Student updatedStudent = studentRepository.save(student);
+
+        return ResponseEntity.ok(updatedStudent);
+    }
+
+    // delet student
+
+    public ResponseEntity<Map<String, Boolean>> deleteStudent(@PathVariable Long id){
+
+        Student student = studentRepository
+                .findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Student not found with id :" +id));
+        studentRepository.delete(student);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return  ResponseEntity.ok(response);
+
+    }
+}
---
+++ student/src/main/java/com/example/student/model/Student.java
@@ -0,0 +1,72 @@
+package com.example.student.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="school_class")
+    private String schoolClass;
+
+    public Student( String firstName, String lastName, String schoolClass) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.schoolClass = schoolClass;
+    }
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSchoolClass() {
+        return schoolClass;
+    }
+
+    public void setSchoolClass(String schoolClass) {
+        this.schoolClass = schoolClass;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", schoolClass='" + schoolClass + '\'' +
+                '}';
+    }
+}
